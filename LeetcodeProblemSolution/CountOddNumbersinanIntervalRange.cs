3927a53a9144780651f641f49d90a914864c5400
HasanMahmudDev <hasanmahmuddev@gmail.com>
2023-03-30 00:18:43 +0600

    Count Odd Number

---
+++ LeetcodeProblemSolution/CountOddNumbersinanIntervalRange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblemSolution
+{
+
+    internal class CountOddNumbersinanIntervalRange
+    {
+        public void CountOddNumbers(int low, int high)
+        {
+            int total = high - low + 1;
+            if(total%2 == 0) 
+            { 
+                Console.WriteLine("Odd number are: "+ total / 2);
+            }
+            else
+            {
+                if (low % 2 == 0)
+                {
+                    Console.WriteLine("Odd number are: " + total / 2);
+                }
+                else
+                {
+                    Console.WriteLine("Odd number are: " + total / 2 + 1);
+                }
+            }
+        }
+    }
+}
--- LeetcodeProblemSolution/Program.cs
+++ LeetcodeProblemSolution/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LeetcodeProblemSolution;
+
 Console.WriteLine("Hello, World!");
+CountOddNumbersinanIntervalRange range = new CountOddNumbersinanIntervalRange();
+    
+    range.CountOddNumbers(3,7);
+    range.CountOddNumbers(8,10);
